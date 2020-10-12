88cabea2f90ac6515ec0d2d36ee7cf283d0e80e7
Jinhee Kim <jinhee.kim@solarconnect.kr>
2020-10-12 20:28:42 +0900

    데이터 추가 표시 및 css 클래스 추가

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Movie({ id, year, title, summary, poster }) {
-  return <h1>{title}</h1>
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title}/>
+      <div className="movie__data">
+        <h1 className="movie__title">{ title }</h1>
+        <span className="movie__title">{ year }</span>
+        <p className="movie__title">{ summary }</p>
+      </div>
+    </div>
+  )
 }
 
 Movie.propType = {
