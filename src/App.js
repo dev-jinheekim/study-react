99869794b79ffa79f50222589be1bdf1b889ae06
Jinhee Kim <jinhee.kim@solarconnect.kr>
2020-10-12 19:23:17 +0900

    movie 데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  componentDidMount () {
+    this.getMovies();
+  }
+
+  async getMovies() {
+    const {
+      data: {
+        data : { movies }
+      }
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    console.log(movies);
+    this.setState({ movies, isLoading: false });
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return <div>{ isLoading ? "Loading..." : movies.map(movie => {
+      return <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.poster}
+      />
+    }) }</div>;
+  };
+
 }
 
 export default App;
